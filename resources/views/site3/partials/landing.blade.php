e1a104c44b86453d77324fb163446b0508bea42f
aurora10 <aurora10@gmail.com>
2020-03-25 20:35:59 +0100

    WWW3333

--- resources/views/site3/partials/landing.blade.php
+++ resources/views/site3/partials/landing.blade.php
@@ -72,36 +72,7 @@ $random_products3 = ProductController::random_products();
 </section>
 <!-- Welcome Slides Area -->
 <!-- Top Catagory Area -->
-<div class="top_catagory_area mt-50 clearfix">
-    <div class="container">
-        <div class="row">
-            <!-- Single Catagory -->
-            <div class="col-12 col-md-4">
-                <div class="single_catagory_area mt-50">
-                    <a href="#">
-                        <img src="img/bg-img/c1.jpg" alt="">
-                    </a>
-                </div>
-            </div>
-            <!-- Single Catagory -->
-            <div class="col-12 col-md-4">
-                <div class="single_catagory_area mt-50">
-                    <a href="#">
-                        <img src="img/bg-img/c2.jpg" alt="">
-                    </a>
-                </div>
-            </div>
-            <!-- Single Catagory -->
-            <div class="col-12 col-md-4">
-                <div class="single_catagory_area mt-50">
-                    <a href="#">
-                        <img src="img/bg-img/c3.jpg" alt="">
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+
 <!-- Top Catagory Area -->
 <!-- Quick View Modal Area -->
 <!-- Quick View Modal Area -->
